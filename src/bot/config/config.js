d23092553c0686b5b7bcae82233bc6fc6877ede5
FarkhodovIslom <111051883+FarkhodovIslom@users.noreply.github.com>
2025-11-22 05:06:13 +0500

    Updated conversation logs and bot configuration

--- src/bot/config/config.js
+++ src/bot/config/config.js
@@ -2,7 +2,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 // Константы
-const PORT = 3000;
+const PORT = 9000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const MAX_HISTORY_LENGTH = 75; // Максимум сообщений в истории
